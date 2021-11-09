2ec47c251b2473fe3543e9fe6cbec5a4e2a1cf99
alexisalmeida <alexis.almeida@gmail.com>
2021-11-09 14:11:34 -0300

    secret_key

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -20,13 +20,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--()4*e%ya@n-^qup_5+gd8$5817*8!pu+c)p-a%at_l)rce3ev'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['alexisralmeida.pythonanywhere.com']
+SECRET_KEY = ''
+
+if DEBUG:
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = 'django-insecure--()4*e%ya@n-^qup_5+gd8$5817*8!pu+c)p-a%at_l)rce3ev'
+else:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
+ALLOWED_HOSTS = ['alexisralmeida.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
